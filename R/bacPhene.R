868942db8b9e83f605fe3f775a9e98872ef821c0
Scott G. Daniel <danielsg@email.chop.edu>
2020-10-19 16:35:31 -0400

    functions for getting specific phenotype data

--- R/bacPhene.R
+++ R/bacPhene.R
@@ -22,10 +22,10 @@
 getStrains <- function(page, genus, species, userpassword) {
 
   api_entry <- 'https://bacdive.dsmz.de/api/bacdive/taxon/'
-  url_species <- URLencode(paste0(api_entry, genus, '/', species, '/?page=', page, '&format=json'))
+  url_species <- utils::URLencode(paste0(api_entry, genus, '/', species, '/?page=', page, '&format=json'))
 
-  response <- getURL(url_species,userpwd=userpassword, httpauth = 1L)
-  jsondata <- fromJSON(response)
+  response <- RCurl::getURL(url_species,userpwd=userpassword, httpauth = 1L)
+  jsondata <- rjson::fromJSON(response)
 
 }
 
@@ -52,10 +52,126 @@ getStrains <- function(page, genus, species, userpassword) {
 #' selection=1,
 #' userpassword = paste0(user,':',passwd))
 #' }
-getStrainData <- function(jsondata, selection=1, userpassword) {
+getStrainData <- function(speciesData, selection=1, userpassword) {
 
-  response <- getURL(paste0(jsondata[["results"]][[selection]][["url"]],'/?format=json'), userpwd=userpassword, httpauth = 1L)
+  response <- RCurl::getURL(paste0(speciesData[["results"]][[selection]][["url"]],'/?format=json'), userpwd=userpassword, httpauth = 1L)
 
-  jsondata <- fromJSON(response)
+  strainData <- rjson::fromJSON(response)
+
+}
+
+#' Gets the gram-stain of the strain
+#'
+#' @param strainData A list from the function \code{\link{getStrainData}}
+#' @param reference Sometimes strains have multiple references for the phenotype information,
+#' this is where you can select the certain reference (or iterate through them)
+#'
+#' @return A character string "negative" or "positive"
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' strain_list <- getStrains(page = 1,
+#' genus = 'Bacteroides',
+#' species = 'xylanisolvens',
+#' userpassword = paste0(user,':',passwd)))
+#' phenotypes_list <- getStrainData(strain_list,
+#' selection=1,
+#' userpassword = paste0(user,':',passwd))
+#' gramStain(phenotypes_list)
+#' }
+gramStain <- function(strainData, reference=1) {
+
+  strainData[["morphology_physiology"]][["cell_morphology"]][[reference]][["gram_stain"]]
+
+}
+
+#' Gets the oxygen tolerance of the strain
+#'
+#' @param strainData A list from the function \code{\link{getStrainData}}
+#' @param reference Sometimes strains have multiple references for the phenotype information,
+#' this is where you can select the certain reference (or iterate through them)
+#'
+#' @return A character string e.g. "aerobe", "anaerobe", etc.
+#' @export
+#'
+#' @examples
+#' #' \dontrun{
+#' strain_list <- getStrains(page = 1,
+#' genus = 'Bacteroides',
+#' species = 'xylanisolvens',
+#' userpassword = paste0(user,':',passwd)))
+#' phenotypes_list <- getStrainData(strain_list,
+#' selection=1,
+#' userpassword = paste0(user,':',passwd))
+#' oxygenTolerance(phenotypes_list)
+#' }
+oxygenTolerance <- function(strainData, reference=1) {
+
+  strainData[["morphology_physiology"]][["oxygen_tolerance"]][[reference]][["oxygen_tol"]]
+
+}
+
+#' Checks whether a bacteria has a specific antibiotic sensitivity
+#'
+#' @param strainData A list from the function \code{\link{getStrainData}}
+#' @param abx A specific antibiotic e.g. "vancomycin"
+#'
+#' @importFrom rlist list.which
+#'
+#' @return TRUE, FALSE or NULL (antibiotic info is not found)
+#' @export
+#'
+#' @examples
+abx_sensitive <- function(strainData, abx = "vancomycin") {
+
+  #TODO: start on this function next (fill in the example for this and abx_resitant) and then write some tests
+
+  abx_list <- strainData[["morphology_physiology"]][["met_antibiotica"]]
+
+  index_num <- rlist::list.which(abx_list, abx %in% .)
+
+  if (length(index_num)!=0) {
+
+    specific_abx <- abx_list[[index_num]]
+
+    specific_abx$ab_sensitive
+
+  } else {
+
+    return(NULL)
+
+  }
+
+}
+
+#' Checks whether a bacteria has a specific antibiotic resistance
+#'
+#' @param strainData A list from the function \code{\link{getStrainData}}
+#' @param abx A specific antibiotic e.g. "vancomycin"
+#'
+#' @importFrom rlist list.which
+#'
+#' @return TRUE, FALSE or NULL (antibiotic info is not found)
+#' @export
+#'
+#' @examples
+abx_resistant <- function(strainData, abx = "vancomycin") {
+
+  abx_list <- strainData[["morphology_physiology"]][["met_antibiotica"]]
+
+  index_num <- rlist::list.which(abx_list, abx %in% .)
+
+  if (length(index_num)!=0) {
+
+    specific_abx <- abx_list[[index_num]]
+
+    specific_abx$ab_resistant
+
+  } else {
+
+    return(NULL)
+
+  }
 
 }
--- tests/testthat/test-bacphene.R
+++ tests/testthat/test-bacphene.R
@@ -32,3 +32,52 @@ test_that("get strain data", {
                               "molecular_biology", "strain_availability", "references"))
 
 })
+
+test_that("gram stain", {
+
+  strain_list <- getStrains(page = 1,
+                            genus = 'Bacteroides',
+                            species = 'xylanisolvens',
+                            userpassword = paste0(user,':',passwd))
+
+  phenotypes_list <- getStrainData(strain_list,
+                                   selection=1,
+                                   userpassword = paste0(user,':',passwd))
+
+  gram_stain <- gramStain(phenotypes_list)
+
+  expect_equal(gram_stain, "negative")
+
+  })
+
+test_that("oxygen tolerance", {
+
+  strain_list <- getStrains(page = 1,
+                            genus = 'Bacteroides',
+                            species = 'xylanisolvens',
+                            userpassword = paste0(user,':',passwd))
+
+  phenotypes_list <- getStrainData(strain_list,
+                                   selection=1,
+                                   userpassword = paste0(user,':',passwd))
+
+  oxygen_tolerance <- oxygenTolerance(phenotypes_list)
+
+  expect_equal(oxygen_tolerance, "anaerobe")
+
+})
+
+test_that("antibiotics sensitive or resistant", {
+
+  strain_list <- getStrains(page = 1,
+                            genus = 'Actibacterium',
+                            species = 'pelagium',
+                            userpassword = paste0(user,':',passwd))
+
+  phenotypes_list <- getStrainData(strain_list,
+                                   selection=1,
+                                   userpassword = paste0(user,':',passwd))
+
+
+
+})
