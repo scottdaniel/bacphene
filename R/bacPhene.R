4e76302f17330d8f90aa26ce19b6ad640afea11d
Scott G. Daniel <danielsg@email.chop.edu>
2020-10-19 14:45:15 -0400

    imports / tests working :)

--- R/bacPhene.R
+++ R/bacPhene.R
@@ -1,16 +1,28 @@
 #' Gets the strains from bacdive that belong to a supplied species
 #'
-#' @param genus
-#' @param species
-#' @param userpassword
+#' @param page You get entries only from the first page (page = '1'), to consider the other pages you have to change the parameter page
+#' @param genus Genus of the species
+#' @param species The species
+#' @param userpassword The userpassword in the form of "user:password" that you got when you registered at bacdive.org
 #'
-#' @return
+#' @return A list of strains from bacdive.org
 #' @export
 #'
+#' @importFrom RCurl getURL
+#' @importFrom rjson fromJSON
+#' @importFrom utils URLencode
+#'
 #' @examples
-getStrains <- function(genus, species, userpassword) {
+#' \dontrun{
+#' getStrains(page = 1,
+#' genus = 'Bacteroides',
+#' species = 'xylanisolvens',
+#' userpassword = paste0(user,':',passwd)))
+#' }
+getStrains <- function(page, genus, species, userpassword) {
 
-  url_species <- URLencode(paste0(taxon,searchterm_genus,'/',searchterm_species,'/?page=',page,'&format=json'))
+  api_entry <- 'https://bacdive.dsmz.de/api/bacdive/taxon/'
+  url_species <- URLencode(paste0(api_entry, genus, '/', species, '/?page=', page, '&format=json'))
 
   response <- getURL(url_species,userpwd=userpassword, httpauth = 1L)
   jsondata <- fromJSON(response)
@@ -19,14 +31,27 @@ getStrains <- function(genus, species, userpassword) {
 
 #' Gets the phenotype data for a specific strain
 #'
-#' @param jsondata
-#' @param selection
-#' @param userpassword
+#' @param jsondata The list object that you got from \code{\link{getStrains}}
+#' @param selection Which strain you want from the list object
+#' @param userpassword The userpassword in the form of "user:password" that you got when you registered at bacdive.org
 #'
-#' @return
+#' @return A list of phenotype data from bacdive.org
 #' @export
 #'
+#' @importFrom RCurl getURL
+#' @importFrom rjson fromJSON
+#' @importFrom utils URLencode
+#'
 #' @examples
+#' \dontrun{
+#' strain_list <- getStrains(page = 1,
+#' genus = 'Bacteroides',
+#' species = 'xylanisolvens',
+#' userpassword = paste0(user,':',passwd)))
+#' getStrainData(strain_list,
+#' selection=1,
+#' userpassword = paste0(user,':',passwd))
+#' }
 getStrainData <- function(jsondata, selection=1, userpassword) {
 
   response <- getURL(paste0(jsondata[["results"]][[selection]][["url"]],'/?format=json'), userpwd=userpassword, httpauth = 1L)
--- tests/testthat/test-bacphene.R
+++ tests/testthat/test-bacphene.R
@@ -1,3 +1,34 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+source("bacdive_credentials.R")
+
+test_that("credentials stored", {
+
+  expect_type(user, "character")
+
+  expect_type(passwd, "character")
+
+})
+
+test_that("get strains", {
+
+  strain_list <- getStrains(page = 1, genus = 'Bacteroides', species = 'xylanisolvens', userpassword = paste0(user,':',passwd))
+
+  expect_named(strain_list, c("count","next","previous","results"))
+
+})
+
+test_that("get strain data", {
+
+  strain_list <- getStrains(page = 1,
+    genus = 'Bacteroides',
+    species = 'xylanisolvens',
+    userpassword = paste0(user,':',passwd))
+
+  phenotypes_list <- getStrainData(strain_list,
+    selection=1,
+    userpassword = paste0(user,':',passwd))
+
+  expect_named(phenotypes_list, c("taxonomy_name", "morphology_physiology", "culture_growth_condition",
+                              "environment_sampling_isolation_source", "application_interaction",
+                              "molecular_biology", "strain_availability", "references"))
+
 })
