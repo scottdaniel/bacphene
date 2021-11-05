bb02a4658080f3cc54b28eb5c37821ba3d584a58
Scott G. Daniel <danielsg@email.chop.edu>
2021-11-05 17:25:51 -0400

    documenting data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Phenotype data of bacterial type strains from bacdive.org
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{ID}{The BacDive ID}
+#'   \item{species}{The species of bacteria}
+#'   \item{aerobic_status}{
+#'     The aerobic status. One of "aerobe", "facultative anaerobe", or
+#'     "obligate anaerobe".}
+#'   \item{gram_stain}{
+#'     How the taxon appears when Gram-stained. One of "Gram-positive" or
+#'     "Gram-negative".}
+#'   \item{doi}{DOI of the publication from which the information was obtained.}
+#' }
+"bacdive_typestrains.rda"
